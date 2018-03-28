e7af3feeb553cc456015a1d6430540351dbd37f3
Shahbaz Javeed <sjaveed@gmail.com>
2018-03-27 20:28:11 -0400

    Postgres: Boolean equality comparisons now use IS TRUE and IS FALSE
    This brings comparisons against booleans in sync with comparisons
    against null values
    
    https://www.postgresql.org/docs/9.3/static/functions-comparison.html

--- test/visitors/test_postgres.rb
+++ test/visitors/test_postgres.rb
@@ -188,6 +188,48 @@ module Arel
         end
       end
 
+      describe 'Nodes::Equality' do
+        it "should escape strings" do
+          test = Table.new(:users)[:name].eq 'Aaron Patterson'
+          compile(test).must_be_like %{
+            "users"."name" = 'Aaron Patterson'
+          }
+        end
+
+        it 'should handle true' do
+          sql = compile Nodes::Equality.new(@table[:active], true)
+          sql.must_be_like %{ "users"."active" IS TRUE }
+        end
+
+        it 'should handle false' do
+          sql = compile Nodes::Equality.new(@table[:active], false)
+          sql.must_be_like %{ "users"."active" IS FALSE }
+        end
+
+        it 'should handle nil' do
+          sql = compile Nodes::Equality.new(@table[:name], nil)
+          sql.must_be_like %{ "users"."name" IS NULL }
+        end
+      end
+
+      describe 'Nodes::NotEqual' do
+        it 'should handle true' do
+          sql = compile Nodes::NotEqual.new(@table[:active], true)
+          sql.must_be_like %{ "users"."active" IS NOT TRUE }
+        end
+
+        it 'should handle false' do
+          sql = compile Nodes::NotEqual.new(@table[:active], false)
+          sql.must_be_like %{ "users"."active" IS NOT FALSE }
+        end
+
+        it 'should handle nil' do
+          val = Nodes.build_quoted(nil, @table[:active])
+          sql = compile Nodes::NotEqual.new(@table[:name], val)
+          sql.must_be_like %{ "users"."name" IS NOT NULL }
+        end
+      end
+
       describe "Nodes::BindParam" do
         it "increments each bind param" do
           query = @table[:name].eq(Arel::Nodes::BindParam.new(1))
